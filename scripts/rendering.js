af29b3895b9ffda96c244de2d0d42c95059f8dc5
IronAgate <ironagate@gmail.com>
2023-12-16 08:46:47 -0600

    using offscreen canvas class

--- scripts/rendering.js
+++ scripts/rendering.js
@@ -10,113 +10,130 @@ class Panel {
 			?need auto option?
 		(func) startup
 			called when user starts the game
-		number layers = 2
-			number of layers to create
 	
 	attributes
-		array(html canvas) figures
-			the canvases you draw to
-		array(html canvas) shadows
-			the canvases presented on page
-		array(canvas ctx) figureContexts
-			contexts for figures
-		array(canvas ctx) shadowContexts
-			contexts for shadows
+		X,
+		Y
+			canvas dimensions
+			from resX, resY
+		bgShadow,
+		bgShadowContext
+			for rendering background
+			the actual display canvas
+		bgFigure,
+		bgFigureContext
+			for rendering the background
+			frame buffer before display
+		fgShadow,
+		fgShadowContext,
+			for rendering foreground
+			actual display canvas
+		fgFigure,
+		figFigureContext
+			for rendering foreground
+			frame buffer before display
+		
 	
 	methods
 		illuminate
-			reveals changes by painting the buffer canvas to the display canvas
+			reveals changes by painting the figure canvas to the shadow canvas
 		pose
 			generic, for drawing to the buffer canvas
 		
 	*/
-	constructor(divId, resX, resY, startup, layers = 2) {
+	constructor(divId, resX, resY, startup) {
 		
-		this._layers = 2;
+		this.X = resX;
+		this.Y = resY;
 		
-		const root = document.getElementById(divId);
-		//style root
-		root.style.aspectRatio = resX/resY;
-		root.style.position = "relative"; //for layering: https://www.shecodes.io/athena/50922-how-to-make-an-absolute-box-responsive-with-css#:~:text=To%20make%20an%20absolute%20position%20box%20responsive%2C%20you%20should%20use,parent%20container%20and%20adjust%20accordingly.
+		const wall = document.getElementById(divId);
+		//style wall
+		wall.style.aspectRatio = resX/resY;
+		wall.style.position = "relative"; //for layering: https://www.shecodes.io/athena/50922-how-to-make-an-absolute-box-responsive-with-css#:~:text=To%20make%20an%20absolute%20position%20box%20responsive%2C%20you%20should%20use,parent%20container%20and%20adjust%20accordingly.
 		
-		const wall = document.createElement("div");
-		wall.style.display = "none";
-		root.appendChild(wall);
-		
-		const figures = [];
-		const shadows = [];
-		const figureContexts = [];
-		const shadowContexts = [];
-		
-		//create canvases
-		for (let i = 0; i < layers; i++) {
+		function makeCanvas(z) {
 			const sh = document.createElement("canvas");
 			sh.width = resX;
 			sh.height = resY;
 			//style
+			sh.style.display = "none";
 			sh.style.width = "100%";
 			sh.style.height = "100%";
-			sh.style.zIndex = i; //layering
+			sh.style.zIndex = z; //layering
 			sh.style.position = "absolute"; //layering
-			
-			shadows[i] = sh;
 			wall.appendChild(sh);
 			
 			const shcx = sh.getContext("2d");
 			shcx.imageSmoothingEnabled = false;
-			shadowContexts[i] = shcx;
 			
-			const fig = document.createElement("canvas");
-			fig.width = resX;
-			fig.height = resY;
-			
+			const fig = new OffscreenCanvas(resX, resY);
 			const figcx = fig.getContext("2d");
 			figcx.imageSmoothingEnabled = false;
-			figureContexts[i] = figcx;
 			
-			figures[i] = fig;
+			return [sh, shcx, fig, figcx];
 		}
 		
-		this._figures = figures;
-		this._figureContexts = figureContexts;
-		this._shadows = shadows;
-		this._shadowContexts = shadowContexts;
-
+		[this.bgShadow, 
+			this.bgShadowContext, 
+			this.bgFigure, 
+			this.bgFigureContext
+		] = makeCanvas(0);
+		[this.fgShadow, 
+			this.fgShadowContext, 
+			this.fgFigure, 
+			this.fgFigureContext
+		] = makeCanvas(1);
 		
+		//start button
 		const btn = document.createElement("button");
 		btn.textContent = "start";
 		btn.id = "startButton";
 		btn.style.width = "100%";
 		btn.style.height = "100%";
+		const bg = this.bgShadow;
+		const fg = this.fgShadow;
 		btn.onclick = 
 			function() {
-				wall.style.display = "block";
+				bg.style.display = "block";
+				fg.style.display = "block";
 				//this.style.display = "none";
 				this.remove();
 				
 				startup();
 			}
-		root.appendChild(btn);	
-		
+		wall.appendChild(btn);	
 	}
 	illuminate() {
-		for (let i = 0; i < this._layers; i++) {
-			this._shadowContexts[i].drawImage(this._figures[i], 0,0);
-		}
+		this.bgShadowContext.drawImage(this.bgFigure, 0,0);
+		this.fgShadowContext.drawImage(this.fgFigure, 0,0);
 	}
-	illuminateOnly(layer=1) {
-		this._shadowContexts[layer].drawImage(this._figures[layer], 0,0);
-		//clear drawing canvas
+	illuminateFg() {
+		this.fgShadowContext.drawImage(this.fgFigure, 0,0);
 	}
-	pose(layer=1) {
-		return this._figureContexts[layer]
+	illuminateBg() {
+		this.bgShadowContext.drawImage(this.bgFigure, 0,0);
+	}
+	poseFg() {
+		return this.fgFigureContext;
+	}
+	poseBg() {
+		return this.bgFigureContext;
+	}
+	
+	poseBgImage(image) {
+		this.bgFigureContext.drawImage(image, 0,0, this.X,this.Y);
 	}
 }
 
-const game = new Panel("cave", 300,300);
+let game;
 
-game.pose(0).fillRect(0,0, 300,300);
+function t_startup() {
+	game.poseBg().fillRect(0,0, 300,300);
+	game.poseFg().fillStyle = "#a05050";
+	game.poseFg().fillRect(125,125, 50,50);
 
-game.pose(1).fillStyle = "#904545";
-game.pose(1).fillRect(10,10, 50,50);
-game.illuminate();
+	game.illuminate()
+}
+
+game = new Panel("cave", 300,300, t_startup);
+
